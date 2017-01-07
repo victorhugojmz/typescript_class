ed99ea2022e59d82af01a1674ac4be1d90410745
Victor Barrios Jimenez <vhbj149601@gmail.com>
2017-01-06 20:04:40 -0600

    changed file name

---
+++ Types/class_1.ts
@@ -0,0 +1,5 @@
+function greeter(person: string): string {
+    return "Hello, " + person;
+}
+var user = "Victor";
+console.log(greeter(user));
--- Types/file.ts
+++
@@ -1,5 +0,0 @@
-function greeter(person: string){
-    return "Hello, " + person;
-}
-var user = "Victor";
-console.log(greeter(user)) 
