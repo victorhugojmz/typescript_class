cd51b1602eef798707505cac4218c1379337976d
Victor Barrios Jimenez <vhbj149601@gmail.com>
2017-01-06 20:28:41 -0600

    Defining variables

--- Types/class_1.ts
+++ Types/class_1.ts
@@ -1,5 +1,19 @@
-function greeter(person: string): string {
-    return "Hello, " + person;
-}
-var user = "Victor";
-console.log(greeter(user));
+/* Tipos de datos */
+/*     Strings  o cadenas de caracteres    */
+let usuario: string;
+/*          Arreglos de strings     */
+let usuarios: string[];
+let imagenes: Array <string>; 
+/*         Number para numeros      */
+let puntaje: number;
+/*         Arreglos de numeros      */
+let puntajes: number[];
+let precios: Array <number>;
+/*       Vartiables tipo Objeto    */
+let cliente: Object; 
+/*    Arreglo de variables tipo objeto   */ 
+let clientes: Object[];
+let Objetos: Array <Object>;
+
+/* usuario = 10; Devuelve un error : "Type 10 is not assignable to type string" */
+
