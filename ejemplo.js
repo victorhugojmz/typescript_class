446deb1b9f17294556ef3d76bb802ad9e01c957b
Victor Barrios Jimenez <vhbj149601@gmail.com>
2017-01-13 01:27:49 -0600

    updating data types

--- ejemplo.js
+++ ejemplo.js
@@ -1,3 +1,8 @@
+/* Mismo ejemplo en TS */
+var mensaje;
+mensaje = "Esta es un mensaje en ES5";
+mensaje = 5;
+var list;
 /* Tipos de datos */
 /*     Strings  o cadenas de caracteres    */
 var usuario;
@@ -12,11 +17,33 @@ var precios;
 /*       Vartiables tipo Objeto    */
 var cliente;
 /*    Arreglo de variables tipo objeto   */
-var clientes;
+/*let clientes: Object[];*/
 var Objetos;
 /* usuario = 10; Devuelve un error : "Type 10 is not assignable to type string" */
-var mensaje;
-var delay_milisegundos;
-delay_milisegundos = 3000;
-mensaje = "mensaje es una variable de tipo string";
-setTimeout(function () { confirm(mensaje); }, delay_milisegundos);
+var nombre_completo;
+var edad;
+nombre_completo = "Leonardo Sanchez";
+edad = 25;
+var presentacion = "\n     Hola! mi nombre completo es: " + nombre_completo + ". \n     Mi edad es " + edad + " anios.  \n  ";
+/* Arreglo de variables de tipo number */
+var numeros;
+var _numeros;
+numeros = [233, 1223, 1020, 122, 220];
+_numeros = [7, Math.PI, 6, 124];
+/* Arreglos de valores de tipo string */
+var nombres;
+var _nombres;
+nombres = []; /* Arreglo vacío */
+_nombres = ["Guadalupe", "Laura", "Michelle"];
+/* Arreglos de valores de tipo any */
+var aleatorios;
+var _aleatorios;
+aleatorios = [false, 7, "Hola Mundo"];
+_aleatorios = [115, "mensaje ennviados", { 'name': "Michelle Marquez" }];
+/* Arreglos de valores de tipo Object  */
+var clientes;
+var _clientes;
+clientes = [];
+_clientes = [{ 'name': "Juan", 'age': 15 }, { 'name': "Silvia", 'age': 25 }];
+var arreglos = "\n\tArreglo de numeros: " + _numeros + "\n\tArreglo de nombres: " + _nombres + "\n\tArreglo de aleatorios: " + aleatorios + "\n\tArreglo de clientes " + _clientes + "\n ";
+console.log(arreglos);
--- ejemplo.ts
+++ ejemplo.ts
@@ -1,3 +1,7 @@
+ /* Mismo ejemplo en TS */ 
+ let mensaje: string;
+ mensaje = "Esta es un mensaje en ES5";
+ /*mensaje = 5; */
 /* Tipos de datos */
 /*     Strings  o cadenas de caracteres    */
 let usuario: string;
@@ -12,11 +16,41 @@ let precios: Array <number>;
 /*       Vartiables tipo Objeto    */
 let cliente: Object; 
 /*    Arreglo de variables tipo objeto   */ 
-let clientes: Object[];
+/*let clientes: Object[];*/
 let Objetos: Array <Object>;
 /* usuario = 10; Devuelve un error : "Type 10 is not assignable to type string" */
- let mensaje: string;
- let delay_milisegundos: number;
- delay_milisegundos = 3000;
- mensaje = "mensaje es una variable de tipo string";
- setTimeout(function(){ confirm(mensaje); },delay_milisegundos);
+ let nombre_completo: string;
+ let edad: number;
+ nombre_completo = "Leonardo Sanchez";
+ edad = 25;
+ let presentacion: string = `
+     Hola! mi nombre completo es: ${ nombre_completo }. 
+     Mi edad es ${ edad } anios.  
+  `;
+ /* Arreglo de variables de tipo number */
+ let numeros: number[ ];
+ let _numeros: Array <number>;
+ numeros = [233,1223,1020,122,220];
+ _numeros = [0b0111,Math.PI,6,124];
+ /* Arreglos de valores de tipo string */
+ let nombres: string[];
+ let _nombres: Array <string>;
+ nombres = [ ]; /* Arreglo vacío */
+ _nombres = ["Guadalupe","Laura","Michelle"]; 
+ /* Arreglos de valores de tipo any */
+ let aleatorios: any[];
+ let _aleatorios: Array <any>;
+ aleatorios = [false,0b0111,"Hola Mundo"];
+ _aleatorios = [115,"mensaje ennviados",{'name': "Michelle Marquez"}];
+ /* Arreglos de valores de tipo Object  */
+ let clientes: Object[];
+ let _clientes: Array <Object>;
+ clientes = [ ];
+ _clientes = [{'name':"Juan", 'age': 15},{'name':"Silvia",'age':25}]; 
+ let arreglos: string =  `
+	Arreglo de numeros: ${ _numeros }
+	Arreglo de nombres: ${ _nombres }
+	Arreglo de aleatorios: ${ aleatorios }
+	Arreglo de clientes ${ _clientes }
+ `;
+ console.log(arreglos);
